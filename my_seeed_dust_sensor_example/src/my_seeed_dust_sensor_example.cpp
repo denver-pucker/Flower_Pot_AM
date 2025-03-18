220c60f22ee82e3fcd633c87948c57baa070e658
denver-pucker <denver_pucker@yahoo.com>
2025-03-18 14:59:32 -0600

    Finished testing air quality sensor and dust sensor

--- my_seeed_dust_sensor_example/src/my_seeed_dust_sensor_example.cpp
+++ my_seeed_dust_sensor_example/src/my_seeed_dust_sensor_example.cpp
@@ -1,5 +1,5 @@
 /* 
- * Project groove_air_quality_sensor
+ * Project my_seed_dust_sensor_example
  * Author: Adrian Montoya
  * Date: 18 MARCH 2025
  */
@@ -13,11 +13,22 @@
 #include "credentials.h"
 
 AirQualitySensor sensor(A0);
+const int dustPin = A1;
 
 int value;          // stores returned value
 int airQuality;     // stores values from grove air quality sensor
 int slopeQuality;   // stores defied slope of grove aqs sensor
 int dust;           // dust value returned
+
+int LPOFAC = 10.0;
+
+float lowPulseOccupancy;
+float ratio;
+float concentration = 0;
+float realConcentration;
+
+unsigned long dustStartTime;
+unsigned long duration;
 
 // Let Device OS manage the connection to the Particle Cloud
 SYSTEM_MODE(SEMI_AUTOMATIC);
@@ -43,9 +54,29 @@ void setup() {
   }
   Serial.printf("\n\n");
 
+  // setup pin modes
+  pinMode(dustPin,INPUT);
+
+  // timing
+  dustStartTime = millis();
 }
 
 void loop() {
+
+  // dust sensor
+  duration = pulseIn(dustPin,LOW);
+  lowPulseOccupancy = lowPulseOccupancy + duration;
+  if((millis() - dustStartTime) >= 30000) {
+    ratio = lowPulseOccupancy / (30000 * LPOFAC);
+    concentration = 1.1*pow(ratio,3)-3.8*pow(ratio,2)+520*ratio+0.62;
+    if(concentration > 1){
+      realConcentration = concentration;
+    }
+    Serial.printf("\nConcentration = %0.5f pcs/0.01cf \n",realConcentration);
+    lowPulseOccupancy = 0;
+    dustStartTime = millis();
+  }
+  
   int quality = sensor.slope();
   Serial.printf("Sensor value: %i\n",sensor.getValue());
   
@@ -61,6 +92,5 @@ void loop() {
   else if (quality == AirQualitySensor::FRESH_AIR) {
     Serial.printf("Fresh air.\n");
   }
-
   delay(1000);
 }
